7af941e504c094176ebc2283ca63258b29312b3c
ramadevi1277 <rongala.ramadevi@gmail.com>
2024-01-18 13:53:11 +0530

    Adding tests for the API testing

--- API_test/delete_request.py
+++ API_test/delete_request.py
@@ -2,7 +2,7 @@ import requests
 
 resp = requests.delete('https://reqres.in/api/users/1000000000000')
 
-print(resp.status_code)
+#print(resp.status_code)
 assert resp.status_code == 204
 
 #we did nit get any response for the delete user
